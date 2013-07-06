454c2baf873296a0d28acbc69d3f1926d93cb994
Brynjar Smari Bjarnason <binni@binnisb.com>
2013-07-06 12:48:20 +0200

    Created and added arguments to parse and defaults as specified in the wiki for CONCOCT

--- src/concoct.py
+++ src/concoct.py
@@ -2,18 +2,19 @@
 import fileinput
 import sys
 import os
-from argparse import ArgumentParser
-
+import re
 import itertools
-from scipy import linalg
-import pylab as pl
-import matplotlib as mpl
-
 
 import pandas as p
 import sklearn.mixture as mixture
-from sklearn import preprocessing
+import pylab as pl
+import matplotlib as mpl
 import numpy as np
+
+from argparse import ArgumentParser,ArgumentTypeError
+from scipy import linalg
+from sklearn import preprocessing
+
 
 
 def main(coverage_file,output_base,n_components,header=None):
@@ -34,24 +35,50 @@ def main(coverage_file,output_base,n_components,header=None):
     
     class_series.to_csv(output_base+"_" +setting+str(gmm.bic(X))+ '.csv')
     
-if __name__=="__main__":
+    
+
+def parse_cluster_list(cc_string):
+    ERROR="'" + cc_string + "' is not a valid range of number. Expected forms like '20,100,2'."
+    try:
+        cc=map(int,cc_string.split(","))
+    except ValueError as e:
+        raise ArgumentTypeError(ERROR)    
+    if not len(cc) == 3:
+        raise ArgumentTypeError(ERROR)
+    first,last,step = cc
+    return xrange(first, last+1, step)
+
+def parse_taxonomy_cluster_list(tax_file):
+    raise NotImplementedError("This functionality has not been added yet. Please use -c and specify range")
+
+def arguments():
     parser = ArgumentParser()
-    parser.add_argument('coverage',
-                        help='specify the coverage file')
-    parser.add_argument('-o','--output',
-                        help='specify the output base file_name, the number of components and cv_type will be added to this file name.')
-    parser.add_argument('--n_start', type=int,
-                        help='The number of clusters to.')
-    parser.add_argument('--header', action='store_true',
-                        help='Use this tag if header is included in coverage file')
-    args = parser.parse_args()
+    parser.add_argument('coverage_file',
+        help='specify the coverage file')
+    parser.add_argument('composition_file',
+        help='specify the composition file')
+    parser.add_argument('-k', type=int, default=4,
+        help='specify kmer length, defaults to tetramer')
 
-    if not args.output:
-       sys.exit(-1) 
-    if args.header:
-        header=0
-    else:
-        header=None
+    parser.add_argument('-r', type=int, default=5,
+        help='How often to initialize each cluster count. default 5 times')
+    parser.add_argument('-i', type=int, default=100,
+        help='Maximum number of iterations if convergance not achieved')
 
-    main(args.coverage,args.output,args.n_start,header)
+    parser.add_argument('-o', 
+        help='specify the output directory, if not provided current directory used. All files will\
+              be created in the folder/CONCOCT_YYMMDD_HHMM folder')
+    
+    #Handle cluster number parsing
+    cluster_count = parser.add_mutually_exclusive_group()
+    cluster_count.add_argument('-c', type=int,nargs="+", default=xrange(20,101,2), type=parse_cluster_list,
+        help='specify range of clusters to try out on format first,last,step. default 20,100,2.')
+    cluster_count.add_argument('-t', type=parse_taxonomy_cluster_list,
+        help='specify a taxonomy file to estimate species number from (X). Will use range X*50%,X*150%,2')
+    
+    return parser.parse_args()
+    
+if __name__=="__main__":
+    args = arguments()
+    print args
 
