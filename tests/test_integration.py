b0e358f442badcc13ac8862e1726daaf8db7c624
Johannes Alneberg <alneberg@kth.se>
2013-07-10 21:07:57 +0200

    Integration tests for checking exit status and that output dir is created

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,34 +1,41 @@
 #!/usr/bin/env python
-from nose.tools import assert_almost_equal, assert_equal
+from nose.tools import assert_almost_equal, assert_equal, assert_true
+from os.path import isdir
 import os
 import sys
-import fileinput
+import subprocess
 
 file_path = os.path.realpath(__file__)
 data_path = os.path.abspath(os.path.join(file_path,"..","..","data/"))
+test_dir_path = os.path.dirname(file_path)
+tmp_dir_path = test_dir_path + '/nose_tmp_output'
 
-class TestMultinomial(object):
+CWD = os.getcwd()
+
+CONCOCT_CALL = """
+CONCOCT -h
+"""
+class TestCMD(object):
     def setUp(self):
-        pass
+        """If tmp dir already exists, delete it"""
+        self.tearDown()
+        os.chdir(test_dir_path)
+        self.c = subprocess.call(CONCOCT_CALL,shell=True)
+
     def tearDown(self):
         """remove temporary output files"""
-        if os.path.isdir('nose_tmp_output'):
-            for f in os.listdir('nose_tmp_output'):
+        if isdir(tmp_dir_path):
+            for f in os.listdir(tmp_dir_path):
                 os.remove(f)
-            os.rmdir('nose_tmp_output')
+            os.rmdir(tmp_dir_path)
 
-    # testing function: log_probability
-    def test_uniform_one_contig_prob(self):
-        
-        f = fileinput.input(os.path.join(data_path,"bambus2.scaffold.linear.fasta.one_contig"))
-        c = list(SeqIO.parse(f,"fasta"))
-        f.close()
-        dna_c = dna.DNA(id = c[0].id, seq = str(c[0].seq))
-        dna_c.calculate_signature()
-        k = 4**4
-        uniform_prob = np.ones((dna.DNA.kmer_hash_count))
-        for i,cnt in dna_c.signature.items():
-            uniform_prob[i] = 1./k
-        log_prob = ml.log_probability(dna_c,uniform_prob)
-        print log_prob
-        assert_almost_equal(log_prob, -3791.05738056)
+    def test_no_errors(self):
+        assert_equal(self.c,0,
+                     msg = "Command exited with nonzero status")
+
+    def test_directory_creation(self):
+        assert_true(isdir(tmp_dir_path),
+                    msg = "Temporary directory not created")
+
+
+
