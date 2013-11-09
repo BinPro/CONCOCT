2110fbc348b6e05c071c32c071ba696bbaa37ca0
Chris Quince <christopher.quince@glasgow.ac.uk>
2013-11-09 18:03:38 +0000

    More distinguishable colours in ClusterPlot.R

--- scripts/ClusterPlot.R
+++ scripts/ClusterPlot.R
@@ -53,10 +53,30 @@ for(c in levels(PCA.df$c))
 colnames(df_ell)[1] <- "x"
 colnames(df_ell)[2] <- "y"
 
+colours <- c("#F0A3FF", "#0075DC", "#993F00","#4C005C","#2BCE48","#FFCC99","#808080","#94FFB5","#8F7C00","#9DCC00","#C20088","#003380","#FFA405","#FFA8BB","#426600","#FF0010","#5EF1F2","#00998F","#740AFF","#990000","#FFFF00");
+
+shapes <- c(15,16,17,18)
+
+nC <- length(colours);
+nS <- length(shapes);
+
+nClust <- length(levels(PCA.df$c))
+
+valuesC <- vector()
+valuesS <- rep(16,nClust);
+
+for(i in 1:nClust){
+	valuesC[i] <- colours[i %% nC + 1] 
+	valuesS[i] <- 16 + i %/% nC
+}
+
+print(valuesC);
+print(valuesS);
+
 pdf(opt$ofile)
 
-p <- ggplot(data=PCA.df, aes(x=x, y=y,colour=c)) + geom_point(size=1.0, alpha=.6) + xlab("PCA1") + ylab("PCA2")
+p <- ggplot(data=PCA.df, aes(x=x, y=y,colour=c)) + geom_point(size=1.0, alpha=.6) + xlab("PCA1") + ylab("PCA2") + scale_colour_manual(values=valuesC) + scale_shape_manual(values=valuesS) + geom_path(data=df_ell, aes(x=x, y=y,colour=as.factor(group)), size=0.5, linetype=2)
 
-if( !is.null(opt$legend)){ p + theme(legend.key.size = unit(0.5, "cm")) + geom_path(data=df_ell, aes(x=x, y=y,colour=as.factor(group)), size=0.5, linetype=2);}else{p + theme(legend.position="none") + geom_path(data=df_ell, aes(x=x, y=y,colour=as.factor(group)), size=0.5, linetype=2);}
+if( !is.null(opt$legend)){ p + theme(legend.key.size = unit(0.5, "cm"));}else{p + theme(legend.position="none");}
 
 dev.off()
