df672ba7e4c57c462fcc7558414927d29666b85a
Brynjar Smari Bjarnason <binni@binnisb.com>
2014-01-16 17:18:35 +0100

    fixed indentations

--- concoct/input.py
+++ concoct/input.py
@@ -9,82 +9,82 @@ from itertools import product, tee, izip
 from Bio import SeqIO
 
 def load_composition(comp_file,kmer_len,threshold):
-        #Composition
-        #Generate kmer dictionary
-        feature_mapping, nr_features = generate_feature_mapping(kmer_len)
-        #Count lines in composition file
-        count_re = re.compile("^>")
-        seq_count = 0
-        with open(comp_file) as fh:
-            for line in fh:
-                if re.match(count_re,line):
-                    seq_count += 1
+    #Composition
+    #Generate kmer dictionary
+    feature_mapping, nr_features = generate_feature_mapping(kmer_len)
+    #Count lines in composition file
+    count_re = re.compile("^>")
+    seq_count = 0
+    with open(comp_file) as fh:
+        for line in fh:
+            if re.match(count_re,line):
+                seq_count += 1
+
+    #Initialize with ones since we do pseudo count, we have i contigs as rows
+    #and j features as columns
+    composition = np.ones((seq_count,nr_features))
     
-        #Initialize with ones since we do pseudo count, we have i contigs as rows
-        #and j features as columns
-        composition = np.ones((seq_count,nr_features))
-        
-        
-        contigs_id = []
-        for i,seq in enumerate(SeqIO.parse(comp_file,"fasta")):
-            contigs_id.append(seq.id)
-            for kmer_tuple in window(seq.seq.tostring().upper(),kmer_len):
-                kmer = "".join(kmer_tuple)
-                if kmer in feature_mapping:
-                    composition[i,feature_mapping[kmer]] += 1
-		
-        composition = p.DataFrame(composition,index=contigs_id,dtype=float)
     
-        # save contig lengths, used for pseudo counts in coverage
-        contig_lengths = composition.sum(axis=1)
+    contigs_id = []
+    for i,seq in enumerate(SeqIO.parse(comp_file,"fasta")):
+        contigs_id.append(seq.id)
+        for kmer_tuple in window(seq.seq.tostring().upper(),kmer_len):
+            kmer = "".join(kmer_tuple)
+            if kmer in feature_mapping:
+                composition[i,feature_mapping[kmer]] += 1
+
+    composition = p.DataFrame(composition,index=contigs_id,dtype=float)
+
+    # save contig lengths, used for pseudo counts in coverage
+    contig_lengths = composition.sum(axis=1)
+
+    # Select contigs to cluster on, namely the sequences longer than the threshold.
+    # The total kmer count without pseudo counts is related to the sequence length through:
+    # 
+    # Kmer_count = Seq_length - kmer_len + 1
+    threshold_filter = composition.sum(axis=1) - nr_features + kmer_len - 1 > threshold
     
-        # Select contigs to cluster on, namely the sequences longer than the threshold.
-	# The total kmer count without pseudo counts is related to the sequence length through:
-	# 
-	# Kmer_count = Seq_length - kmer_len + 1
-        threshold_filter = composition.sum(axis=1) - nr_features + kmer_len - 1 > threshold
-        
-        #log(p_ij) = log[(X_ij +1) / rowSum(X_ij+1)]
-        composition = np.log(composition.divide(composition.sum(axis=1),axis=0))
-        
-        logging.info('Successfully loaded composition data.')
-        return composition,contig_lengths,threshold_filter
+    #log(p_ij) = log[(X_ij +1) / rowSum(X_ij+1)]
+    composition = np.log(composition.divide(composition.sum(axis=1),axis=0))
+    
+    logging.info('Successfully loaded composition data.')
+    return composition,contig_lengths,threshold_filter
 
 def load_coverage(cov_file, contig_lengths, no_cov_normalization, add_total_coverage=False):
-        #Coverage import, file has header and contig ids as index
-        #Assume datafile is in coverage format without pseudo counts
-        cov = p.read_table(cov_file,header=0,index_col=0)
+    #Coverage import, file has header and contig ids as index
+    #Assume datafile is in coverage format without pseudo counts
+    cov = p.read_table(cov_file,header=0,index_col=0)
 
-        temp_cov_range = None
-	# cov_range variable left here for historical reasons. Can be removed entirely
-	cov_range = (cov.columns[0],cov.columns[-1])
+    temp_cov_range = None
+    # cov_range variable left here for historical reasons. Can be removed entirely
+    cov_range = (cov.columns[0],cov.columns[-1])
 
-        # Adding pseudo count
-        cov.ix[:,cov_range[0]:cov_range[1]] = cov.ix[:,cov_range[0]:cov_range[1]].add(
-                (100/contig_lengths),
-                axis='index')
-	if add_total_coverage:
-	    cov['total_coverage'] = cov.ix[:,cov_range[0]:cov_range[1]].sum(axis=1)
-	    temp_cov_range = (cov_range[0],'total_coverage')
+    # Adding pseudo count
+    cov.ix[:,cov_range[0]:cov_range[1]] = cov.ix[:,cov_range[0]:cov_range[1]].add(
+            (100/contig_lengths),
+            axis='index')
+    if add_total_coverage:
+        cov['total_coverage'] = cov.ix[:,cov_range[0]:cov_range[1]].sum(axis=1)
+        temp_cov_range = (cov_range[0],'total_coverage')
 
-	if not no_cov_normalization:
-            #Normalize per sample first
-	    cov.ix[:,cov_range[0]:cov_range[1]] = \
-		_normalize_per_sample(cov.ix[:,cov_range[0]:cov_range[1]])
+    if not no_cov_normalization:
+        #Normalize per sample first
+        cov.ix[:,cov_range[0]:cov_range[1]] = \
+            _normalize_per_sample(cov.ix[:,cov_range[0]:cov_range[1]])
 
-            # Normalize contigs next
-            cov.ix[:,cov_range[0]:cov_range[1]] = \
-		_normalize_per_contig(cov.ix[:,cov_range[0]:cov_range[1]])
+        # Normalize contigs next
+        cov.ix[:,cov_range[0]:cov_range[1]] = \
+            _normalize_per_contig(cov.ix[:,cov_range[0]:cov_range[1]])
 
-	if temp_cov_range:
-            cov_range = temp_cov_range
+    if temp_cov_range:
+        cov_range = temp_cov_range
 
-        # Log transform
-        cov.ix[:,cov_range[0]:cov_range[1]] = np.log(
-            cov.ix[:,cov_range[0]:cov_range[1]])
-    
-        logging.info('Successfully loaded coverage data.')
-        return cov,cov_range
+    # Log transform
+    cov.ix[:,cov_range[0]:cov_range[1]] = np.log(
+        cov.ix[:,cov_range[0]:cov_range[1]])
+
+    logging.info('Successfully loaded coverage data.')
+    return cov,cov_range
     
 def _normalize_per_sample(arr):
     """ Divides respective column of arr with its sum. """
