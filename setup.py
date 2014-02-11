13bae402e80f6882fd0277e69525a44554e0aa65
Johannes Alneberg <johannesalneberg@gmail.com>
2014-02-11 18:26:10 +0100

    Bumped version to 0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 import sys, os
 from distutils.core import Extension
 
-version = '0.1'
+version = '0.2'
 
 module1 = Extension('vbgmm',
         libraries =['gsl',  'gslcblas'],
