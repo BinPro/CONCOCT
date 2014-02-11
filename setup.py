dd81ee270c55781b016855acfe257afb6be209c6
Johannes Alneberg <johannesalneberg@gmail.com>
2014-02-11 14:29:24 +0100

    Added long description to setup.py

--- setup.py
+++ setup.py
@@ -12,8 +12,10 @@ module1 = Extension('vbgmm',
 setup(name='concoct',
       version=version,
       description="Clustering cONtigs with COverage and ComposiTion",
-      long_description="""\
-To be done""",
+      long_description="""Concoct is a program that combines three types of 
+      information - sequence composition, coverage across multiple sample, 
+      and read-pair linkage - to automatically bin metagenomic contigs 
+      into genomes. """,
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='Python Scilifelab Metagenomics Binning Clustering Contig',
       author='Brynjar Smari Bjarnason, Johannes Alneberg, Christopher Quince, Anders Andersson, Ino de Bruijn',
