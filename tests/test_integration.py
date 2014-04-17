fbf9c8d8214fc9a58e640f93f77ac547d892cc75
Brynjar Smari Bjarnason <binni@binnisb.com>
2014-04-17 12:38:56 +0200

    had to adust these numbers since they are affected by the contig length coverage bug #65

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -289,7 +289,7 @@ class TestCMD(object):
         # true pseudo count found by running once and using that. Previous tests
         # when we did not normalize the coverage were calculated by hand and 
         # were correct so I assume this is correct with normalization
-        true_pseudo_cov = -1.3062 
+        true_pseudo_cov = -1.3115 
         calc_pseudo_cov = df.sample_1[0]
         assert_almost_equal(true_pseudo_cov,calc_pseudo_cov,places=4)
 
@@ -299,7 +299,7 @@ class TestCMD(object):
         df = p.io.parsers.read_table(original_coverage_data_path,index_col=0,sep=',')
         # Manually calculated pseudo coverage using
         # coverage 0.153531, contig length 10132
-        true_pseudo_cov = -1.8115 
+        true_pseudo_cov = -1.8107 
         calc_pseudo_cov = df.sample_1[0]
         assert_almost_equal(true_pseudo_cov,calc_pseudo_cov,places=4)
 
