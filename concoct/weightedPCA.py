df452749f3f2ebb1ecd7519de0c3fe79625f3071
Chris Quince <chrisq@quince-srv2.eng.gla.ac.uk>
2014-04-30 22:40:14 +0100

    added class for weighted PCA

---
+++ concoct/weightedPCA.py
@@ -0,0 +1,201 @@
+import numpy as np
+from scipy import linalg
+from math import log
+import warnings
+
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.utils import array2d, check_random_state, as_float_array
+from sklearn.utils import atleast2d_or_csr
+
+class weightedPCA(BaseEstimator, TransformerMixin):
+    """Principal component analysis (PCA)
+
+    Linear dimensionality reduction using Singular Value Decomposition of the
+    data and keeping only the most significant singular vectors to project the
+    data to a lower dimensional space.
+
+    This implementation uses the scipy.linalg implementation of the singular
+    value decomposition. It only works for dense arrays and is not scalable to
+    large dimensional data.
+
+    The time complexity of this implementation is ``O(n ** 3)`` assuming
+    n ~ n_samples ~ n_features.
+
+    Parameters
+    ----------
+    n_components : int, None or string
+        Number of components to keep.
+        if n_components is not set all components are kept::
+
+            n_components == min(n_samples, n_features)
+    copy : bool
+        If False, data passed to fit are overwritten and running
+        fit(X).transform(X) will not yield the expected results,
+        use fit_transform(X) instead.
+
+    Attributes
+    ----------
+    `components_` : array, [n_components, n_features]
+        Components with maximum variance.
+
+    `explained_variance_ratio_` : array, [n_components]
+        Percentage of variance explained by each of the selected components. \
+        k is not set then all components are stored and the sum of explained \
+        variances is equal to 1.0
+
+    Due to implementation subtleties of the Singular Value Decomposition (SVD),
+    which is used in this implementation, running fit twice on the same matrix
+    can lead to principal components with signs flipped (change in direction).
+    For this reason, it is important to always use the same estimator object to
+    transform data in a consistent fashion.
+
+    Examples
+    --------
+
+    >>> import numpy as np
+    >>> from sklearn.decomposition import PCA
+    >>> X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+    >>> pca = PCA(n_components=2)
+    >>> pca.fit(X)
+    PCA(copy=True, n_components=2, whiten=False)
+    >>> print(pca.explained_variance_ratio_) # doctest: +ELLIPSIS
+    [ 0.99244...  0.00755...]
+
+    See also
+    --------
+    ProbabilisticPCA
+    RandomizedPCA
+    KernelPCA
+    SparsePCA
+    TruncatedSVD
+    """
+    def __init__(self, n_components=None, copy=True):
+        self.n_components = n_components
+        self.copy = copy
+
+    def fit(self, X, weights, y=None):
+        """Fit the model with X.
+
+        Parameters
+        ----------
+        X: array-like, shape (n_samples, n_features)
+            Training data, where n_samples in the number of samples
+            and n_features is the number of features.
+
+        Returns
+        -------
+        self : object
+            Returns the instance itself.
+        """
+        self._fit(X,weights)
+        return self
+
+    def fit_transform(self, X, weights, y=None):
+        """Fit the model with X and apply the dimensionality reduction on X.
+
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_features)
+            Training data, where n_samples is the number of samples
+            and n_features is the number of features.
+
+        Returns
+        -------
+        X_new : array-like, shape (n_samples, n_components)
+
+        """
+        U, S, V = self._fit(X,weights)
+        U = U[:, :self.n_components]
+	# X_new = X * V = U * S * V^T * V = U * S
+        U *= S[:self.n_components]
+        return U
+
+    def _fit(self, X, weights):
+        """ Fit the model on X
+        Parameters
+        ----------
+        X: array-like, shape (n_samples, n_features)
+            Training vector, where n_samples in the number of samples and
+            n_features is the number of features.
+	weights: vector like relative sample weights
+        Returns
+        -------
+        U, s, V : ndarrays
+            The SVD of the input data, copied and centered when
+            requested.
+        """
+        X = array2d(X)
+        n_samples, n_features = X.shape
+        X = as_float_array(X, copy=self.copy)
+	# Center data
+	Y = np.dot(np.diag(weights),X)
+        self.mean_ = np.mean(Y, axis=0)
+        X -= self.mean_
+
+        sweights = np.sqrt(weights)
+	Z = np.dot(np.diag(sweights),X)
+
+        U, S, V = linalg.svd(Z, full_matrices=False)
+	
+	tweight = np.sum(weights)
+        self.explained_variance_ = (S ** 2) / tweight
+        self.explained_variance_ratio_ = (self.explained_variance_ /
+                                          self.explained_variance_.sum())
+
+        if (self.n_components is not None
+              and 0 < self.n_components
+              and self.n_components < 1.0):
+            # number of components for which the cumulated explained variance
+            # percentage is superior to the desired threshold
+            ratio_cumsum = self.explained_variance_ratio_.cumsum()
+            self.n_components = np.sum(ratio_cumsum < self.n_components) + 1
+
+        if self.n_components is not None:
+            self.components_ = self.components_[:self.n_components, :]
+            self.explained_variance_ = \
+                self.explained_variance_[:self.n_components]
+            self.explained_variance_ratio_ = \
+                self.explained_variance_ratio_[:self.n_components]
+
+        return (U, S, V)
+
+    def transform(self, X):
+        """Apply the dimensionality reduction on X.
+
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_features)
+            New data, where n_samples is the number of samples
+            and n_features is the number of features.
+
+        Returns
+        -------
+        X_new : array-like, shape (n_samples, n_components)
+
+        """
+        X = array2d(X)
+        if self.mean_ is not None:
+            X = X - self.mean_
+        X_transformed = np.dot(X, self.components_.T)
+        return X_transformed
+
+    def inverse_transform(self, X):
+        """Transform data back to its original space, i.e.,
+        return an input X_original whose transform would be X
+
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_components)
+            New data, where n_samples is the number of samples
+            and n_components is the number of components.
+
+        Returns
+        -------
+        X_original array-like, shape (n_samples, n_features)
+
+        Notes
+        -----
+        If whitening is enabled, inverse_transform does not compute the
+        exact inverse operation as transform.
+        """
+        return np.dot(X, self.components_) + self.mean_
