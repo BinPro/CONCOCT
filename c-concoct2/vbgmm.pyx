1fc0636591734ba455f367673d261373efe6f270
Christopher Quince <u1375038@004851.home>
2016-06-04 17:27:58 +0100

    Fixed return vals

--- c-concoct2/vbgmm.pyx
+++ c-concoct2/vbgmm.pyx
@@ -16,9 +16,9 @@ cdef extern void c_vbgmm_fit (double* adX, int nN, int nD, int nK, int* anAssign
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def fit(np.ndarray[double, ndim=2, mode="c"] xarray not None, np.ndarray[int, ndim=1, mode="c"] assign not None, nClusters, debug):
+def fit(np.ndarray[double, ndim=2, mode="c"] xarray not None, nClusters, debug):
     """
-    fit (xarray, assign, nK)
+    fit (xarray, assign, nK, debug)
 
     Takes a numpy array xarray as input, fits the vbgmm using nK initial clusters
 
@@ -28,12 +28,14 @@ def fit(np.ndarray[double, ndim=2, mode="c"] xarray not None, np.ndarray[int, nd
     param: assigns -- cluster assignments must have same number of rows as xarray
 
     """
-    cdef int nN, nD, nK
-
+    cdef int nN, nD, nK, nS
+        
     nN, nD = xarray.shape[0], xarray.shape[1]
 
     nK = nClusters
-
+    
+    cdef np.ndarray[int, ndim=1,mode="c"] assign = np.zeros((nN), dtype=np.intc)
+    
     c_vbgmm_fit (&xarray[0,0], nN, nD, nK, &assign[0], debug)
 
-    return None
+    return assign
